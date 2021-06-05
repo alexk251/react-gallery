1bd79184ec048e194d3a99a3686d3a47217605cb
Alex Kim <alexkim251@yahoo.com>
2021-06-05 15:50:38 -0500

    got get route working with sql database

--- database.sql
+++ database.sql
@@ -8,7 +8,7 @@ CREATE TABLE "gallery" (
 INSERT INTO "gallery"
 	("path", "description")
 VALUES
-	('ttps://media.musiciansfriend.com/is/image/MMGS7/L46959000001000-00-500x500.jpg', 'Strandberg Boden Standard 6 Electric Guitar  Black Flame'),
+	('https://media.musiciansfriend.com/is/image/MMGS7/L46959000001000-00-500x500.jpg', 'Strandberg Boden Standard 6 Electric Guitar  Black Flame'),
     ('https://cdn.shopify.com/s/files/1/0296/7607/7115/products/ABASILARADA8CHARCOAL4800FLAT_5000x.jpg?v=1602378893', 'LARADA 8 // LEGION // CHARCOAL BURL'),
     ('https://lh3.googleusercontent.com/proxy/GqNc-qAa1v5Y_nBUOiAfkgpR01S8Ar5V4lD6VuDs58QkPkvspRQ6Cj2ZYZr-93bLo38F8oCTuVllpqwQbYbCD2ndYzDh9uDO5mLLb3FelIy3l4Q', 'Klein Guitars - Klein Guitars - Ed Roman King of Guitars'),
     ('https://cdn11.bigcommerce.com/s-dks6ju/images/stencil/1280x1280/products/9808/93151/p0812021_01__31496.1459973812.jpg?c=2', '2008 PARKER FLY MOJO ELECTRIC GUITAR QUILT TOP GREEN SUNBURST'), 
--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    // console.log(req.params);
+    // const galleryId = req.params.id;
+    // for(const galleryItem of galleryItems) {
+    //     if(galleryItem.id == galleryId) {
+    //         galleryItem.likes += 1;
+    //     }
+    // }
+    // res.sendStatus(200);
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+
+    const queryText = `SELECT * FROM "gallery" ORDER BY "id";`
+
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(err => {
+        console.log(err)
+        res.sendStatus(500);
+    })
+
+    
 }); // END GET Route
 
 module.exports = router;
