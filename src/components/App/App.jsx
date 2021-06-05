49c4f5a651978f8832ecac4a00ffb4a3e633f601
Alex Kim <alexkim251@yahoo.com>
2021-06-05 10:56:09 -0500

    added get axious route and useState to trigger the axios get on page load

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [gallery, setGallery] = useState([]);
+
+useEffect(() => {
+  getGallery()
+}, [])
+
+const getGallery = () => {
+    console.log('inside getGallery')
+    axios.get('/gallery').then(response => {
+        setGallery(response.data)
+    }).catch(err => {
+      console.error(err)
+    })
+}
+
     return (
       <div className="App">
         <header className="App-header">
