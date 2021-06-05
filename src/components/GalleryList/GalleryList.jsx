215dc1d447e7fa72d399998beb75cf13e0f26adc
Alex Kim <alexkim251@yahoo.com>
2021-06-05 11:14:16 -0500

    added Gallery List component ready to send data to GalleryItem to assist with render

---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,17 @@
+import GalleryItem from "../GalleryItem/GalleryItem"
+
+function GalleryList ({gallery, getGallery}) {
+    // renders and loops gallery in a list of components
+    return (
+    <div className="flex">
+        {gallery.map(item => {
+            //each item gets sent to GalleryItem
+            return (
+                <GalleryItem getGallery={getGallery} key={item.id} item={item} />
+            )
+        })}
+    </div>
+    )
+}
+
+export default GalleryList
