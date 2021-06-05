ba9ab4fec513574977ea4454b36e2ebcb5c75e34
Alex Kim <alexkim251@yahoo.com>
2021-06-05 12:28:19 -0500

    added images to server gallery.data.js file

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,10 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'https://media.musiciansfriend.com/is/image/MMGS7/L46959000001000-00-500x500.jpg', description: 'Designed to bring Strandberg\'s unique concepts to a solidbody line that combines cutting edge technology with outstanding value, the new Strandberg Boden Standard 6 brings you a headless, multi-scale guitar with the unique, ergonomic EndurNeck profile and EGS headless fixed bridge with string locking tuners.', likes: 0 },
-    { id: 2, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 4, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 5, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 6, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'https://media.musiciansfriend.com/is/image/MMGS7/L46959000001000-00-500x500.jpg', description: 'Strandberg Boden Standard 6 Electric Guitar  Black Flame', likes: 0 },
+    { id: 2, path: 'https://cdn.shopify.com/s/files/1/0296/7607/7115/products/ABASILARADA8CHARCOAL4800FLAT_5000x.jpg?v=1602378893', description: 'LARADA 8 // LEGION // CHARCOAL BURL', likes: 0 },
+    { id: 3, path: 'https://lh3.googleusercontent.com/proxy/GqNc-qAa1v5Y_nBUOiAfkgpR01S8Ar5V4lD6VuDs58QkPkvspRQ6Cj2ZYZr-93bLo38F8oCTuVllpqwQbYbCD2ndYzDh9uDO5mLLb3FelIy3l4Q', description: 'Klein Guitars - Klein Guitars - Ed Roman King of Guitars', likes: 0 },
+    { id: 4, path: 'https://cdn11.bigcommerce.com/s-dks6ju/images/stencil/1280x1280/products/9808/93151/p0812021_01__31496.1459973812.jpg?c=2', description: '2008 PARKER FLY MOJO ELECTRIC GUITAR QUILT TOP GREEN SUNBURST', likes: 0 },
+    { id: 5, path: 'https://c1.zzounds.com/media/productmedia/fit,600by800/quality,85/1_Full_Straight_Front_NA-b166a1a6240fd86f6ec781519ee946f4.jpg', description: 'Guild A-150 Savoy Hollowbody Electric Guitar ', likes: 0 },
+    { id: 6, path: 'https://c1.zzounds.com/media/productmedia/fit,600by800/quality,85/1_Full_Straight_Front_NA-c5be6f3ff3b526c00ccfe5fc045c2086.jpg', description: 'Gibson ES-335 Figured Electric Guitar', likes: 0 }
 ];
 
 module.exports = galleryItems;
